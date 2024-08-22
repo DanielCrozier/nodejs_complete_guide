90f53ddbfc6077640660bc8605f98e3fc8b07847
Daniel Crozier <daniel.crozier01@icloud.com>
2024-08-22 19:39:58 -0400

    69. Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   res.send(`
 <html lang="en">
-<form action="/product" method="POST">
+<form action="/admin/add-product" method="POST">
     <input type="text" name="title">
     <button type="submit">Add</button>
 </form>    
@@ -13,7 +14,8 @@ router.get('/add-product', (req, res, next) => {
 `);
 })
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
