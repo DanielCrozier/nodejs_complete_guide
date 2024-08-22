424cce5c788af5a682c2b301f038fd0dfbffde3c
Daniel Crozier <daniel.crozier01@icloud.com>
2024-08-22 19:30:31 -0400

    67. Using Express Router

--- server.cjs
+++ server.cjs
@@ -1,35 +1,15 @@
-const http = require( 'http' );
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 // Start Express.js middleware functions
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(`
-<html lang="en">
-<form action="/product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add</button>
-</form>    
-</html>
-`);
-})
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello, World!</h1>');
-})
-
-// const server = http.createServer(app);
-//
-// server.listen(3000);
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
