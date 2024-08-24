50883b40a279584a834742d83670af69031e84fd
Daniel Crozier <daniel.crozier01@icloud.com>
2024-08-24 14:07:45 -0400

    87. Finishing the Pug Template

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product', paht: '/admin/add-product' });
 })
 
 // /admin/add-product => POST
