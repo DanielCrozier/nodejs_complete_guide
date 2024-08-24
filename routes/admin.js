eccee849f180b240bbb634c1553c9cd86b8f013e
Daniel Crozier <daniel.crozier01@icloud.com>
2024-08-24 09:16:28 -0400

    Added exports object with products array and router

--- routes/admin.js
+++ routes/admin.js
@@ -6,15 +6,18 @@ const rootDir = require('../util/path')
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
+router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
