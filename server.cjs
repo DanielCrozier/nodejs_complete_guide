bb9daa70c6ba83aef5530a6a5c2f8c64145f5649
Daniel Crozier <daniel.crozier01@icloud.com>
2024-09-17 16:48:55 -0400

    100. Adding a Product Model

--- server.cjs
+++ server.cjs
@@ -10,6 +10,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 // Start Express.js middleware functions
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,12 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: 'Page Not Found',
-    path:'404'
-  });
-})
+app.use(errorController.get404);
 
 app.listen(3000);
 
